c1cebb17eba9789b6c39c6ddc6ee4b260ac72fe9
Matix426 <kinder0011@wp.pl>
2017-09-25 12:35:57 +0200

    Obliczanie kwadratu

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -8,7 +8,19 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	cout << "Witaj :D";
+	//char imie; // delaracja zmiennej znakowej
+	char imie[10]; // delaracja zmiennej tablicowej
+    int wiek; 
+    wiek = 0; //inicjacja zmiennej 
+    cout << "Witaj :D" << endl;
+	cout << "Podaj imię:";
+    // cin >> imie;
+    cin.getline(imie, 10); 
+    cout << "Cześć, " <<imie <<"!" <<endl;
+    cout << "Ile masz lat? ";
+    cin >> wiek; 
+    cout <<"Urodziłeś się w roku " << 2017 - wiek <<endl; 
+    
 	return 0;
 }
 
