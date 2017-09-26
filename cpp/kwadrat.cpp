d799d8fab92551529b80de80c829774c61f5ea6b
Matix426 <kinder0011@wp.pl>
2017-09-26 09:56:29 +0200

    Kwadrat

---
+++ cpp/kwadrat.cpp
@@ -0,0 +1,19 @@
+/*
+ * hello.cpp
+*/
+
+
+#include <iostream>
+using namespace std; 
+
+int main(int argc, char **argv)
+{
+    int bok =0; 
+    cout <<"Podaj bok kwadratu " <<endl;
+    cin >> bok;
+    cout <<"Pole wynosi " << bok * bok <<endl;
+    cout <<"Obwód wynosi " << 4 *bok <<endl;
+    
+	return 0;
+}
+
