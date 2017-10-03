fb29a7b21412cb7553930892939a8fa9bfac12b1
Matix426 <kinder0011@wp.pl>
2017-10-03 09:55:15 +0200

    Sumuj

--- cpp/sumuj.cpp
+++ cpp/sumuj.cpp
@@ -5,25 +5,22 @@
 
 #include <iostream>
 
+#include <iostream>
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    int i; 
-    int j;
-    
-    for (i = 1; i < 101; i++)
-    {
-        cout << '*';
-        if (i % 10 == 0)
-        {
-            for (j=0; j<9; j++)
-                cout << '#';
-            cout << i << endl;
-        }
-        
-    }
-    
-    return 0;
+    int i;
+    int wynik=0;
+
+    for(i =1; i < 11; i++)
+{
+    wynik+=i;
+
+}
+cout << wynik;
+
+	return 0;
 }
 
