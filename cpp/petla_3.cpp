5b4c0fedc736b93b434e0b1ddc34d1f537b8e989
Matix426 <kinder0011@wp.pl>
2017-10-09 12:34:06 +0200

    Petle cd.

--- cpp/petla_3.cpp
+++ cpp/petla_3.cpp
@@ -13,12 +13,13 @@ int main(int argc, char **argv)
     int suma=0;
     int liczba=0;
     
-    for(i =0; i < 10; i++)
+    for(i =0; i < 75; i++)
     {
     cout <<"Podaj liczbe: ";
     cin >> liczba;
     suma+=liczba;
-    
+    if (suma > 75)
+            break; 
 
     }   
     cout <<"Suma liczb: " <<suma <<endl;
---
+++ cpp/petla_5.cpp
@@ -0,0 +1,19 @@
+/*
+ * petla_5.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+
+{
+    int n;
+    cin>>n;
+    for (int i=0; i<=n; i++)
+    cout <<i*i<<endl;
+	
+	return 0;
+}
+
