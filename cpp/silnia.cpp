927dcc8be98b0bef65865d868476960abe0a37df
Matix426 <kinder0011@wp.pl>
2017-11-14 17:21:51 +0100

    Silnia

--- cpp/silnia.cpp
+++ cpp/silnia.cpp
@@ -5,9 +5,20 @@
 
 #include <iostream>
 
-int main(int argc, char **argv)
+using namespace std;
+ 
+int main()
 {
-	
-	return 0;
+    int a;
+    int wynik = 1;
+ 
+    cout << "Podaj liczbę naturalną: ";
+    cin >> a;
+ 
+    for (int i = 1; i<=a;i++)
+        wynik*= i;
+ 
+    cout <<"Silnia: " <<wynik << endl;
+ 
+    return(0);
 }
-
