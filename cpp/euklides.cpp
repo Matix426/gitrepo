b52cc594150bcdab23c077d59b512be1aac8dc1c
Matix426 <kinder0011@wp.pl>
2017-11-20 16:47:55 +0100

    Euklides

---
+++ cpp/euklides.cpp
@@ -0,0 +1,35 @@
+/*
+ * euklides.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int nww(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b)
+        a = a - b;
+        else
+        b = b - a;
+    }
+        return a;
+}
+
+int main(int argc, char **argv)
+{
+    int a,b;
+    cout << "Podaj liczbę: ";
+    cin >> a;
+    cout << "Podaj liczbę: ";
+    cin >> b;
+
+    cout << "Największy wspólny dzielnik (" << a << "," << b << ") = " <<nww(a,b)<<endl;
+    
+    
+return 0;
+}
+
