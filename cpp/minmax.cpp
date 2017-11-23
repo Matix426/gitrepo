86aa260b0848f628dcfbb569d964f7cc6bd14970
Matix426 <kinder0011@wp.pl>
2017-11-23 19:35:35 +0100

    Minmax

---
+++ cpp/minmax.cpp
@@ -0,0 +1,13 @@
+/*
+ * minmax.cpp
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
--- python/minmax.py
+++ python/minmax.py
@@ -6,21 +6,22 @@
 
 import random
 
+
 def losuj(ileliczb, maksliczb):
     liczby = []  # pusta lista
 
     ile = 0  # ilość unikalnych liczb
     # for i in range(ileliczb):
+
     while ile < ileliczb:
         liczba = random.randint(0, maksliczb)
         if liczby.count(liczba) == 0:
             liczby.append(liczba)
             ile += 1
-
-    # print(liczby)
-    print(lista)
+    print(liczby)
     return liczby
-    
+
+
 def minimum(lista):
     """Wyszukiwanie minimum"""
     min = lista[0]
@@ -30,9 +31,10 @@ def minimum(lista):
 
 
 def main(args):
-    lista = losuj(20,50)
+    lista = losuj(20, 50)
     print("Min: ", minimum(lista))
     return 0
+
 
 if __name__ == '__main__':
     import sys
