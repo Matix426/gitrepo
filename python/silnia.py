2feeae84f9eada39b3a94a82f7a3a308b042750b
Matix426 <kinder0011@wp.pl>
2017-11-27 12:35:58 +0100

    Fibonacci

--- python/silnia.py
+++ python/silnia.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 #
 #  silnia.py
-#  n! = 1 dla n={0, 1}   
+#  n! = 1 dla n={0,1}
 #  n! = 1 * ... * n dla N+ - {1}
 
 def silnia_it(n):
     wynik =1
-    for i in range(2, n+1): 
+    for i in range(2, n+1):
         wynik = wynik*i
     return wynik
 
@@ -15,10 +15,12 @@ def main(args):
     n = int(input('Podaj liczbę naturalną: '))
     assert type(n) == int
     assert silnia_it(0) == 1
-    assert silnia_it(1) == 1
-    assert silnia_it(2) == 2
-    assert silnia_it(3) == 6
-    # print('Silnia dla {:d}: {:d}', .format(n, silnia_it(n))
+
+
+
+    print('Silnia: ', silnia_it(n))
+
+
     return 0
 
 if __name__ == '__main__':
