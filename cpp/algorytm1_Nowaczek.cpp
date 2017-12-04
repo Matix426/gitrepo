6271aba354f651576dc4b499f88823dc2c168513
Matix426 <kinder0011@wp.pl>
2017-12-04 12:40:18 +0100

    Cw

---
+++ cpp/algorytm1_Nowaczek.cpp
@@ -0,0 +1,35 @@
+/*
+ * algorytm1
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int n;
+    int a;
+    int i;
+    int iloczyn = 1
+    
+    cout <<"Podaj n: ";
+    cin >> n;
+    cout <<"Podaj a: ";
+    cin >> a;
+       
+        for(i=1; i=n; i++)
+        {
+            if(i = n)
+                iloczyn = iloczyn * n;
+            else
+                i = i+1;
+        }
+        return iloczyn;
+        
+            
+
+
+}
+    
+    
+
