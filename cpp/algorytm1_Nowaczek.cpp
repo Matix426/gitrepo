37befc4c77c0cad38ef9d92d4de7d9c8a19e527b
Matix426 <kinder0011@wp.pl>
2017-12-04 19:51:30 +0100

    Cw

--- cpp/algorytm1_Nowaczek.cpp
+++ cpp/algorytm1_Nowaczek.cpp
@@ -5,31 +5,24 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char **argv)
 {
-    int n;
-    int a;
-    int i;
-    int iloczyn = 1
-    
+    int a,n;
+    int iloczyn=1;
     cout <<"Podaj n: ";
-    cin >> n;
-    cout <<"Podaj a: ";
-    cin >> a;
-       
-        for(i=1; i=n; i++)
-        {
-            if(i = n)
-                iloczyn = iloczyn * n;
-            else
-                i = i+1;
-        }
-        return iloczyn;
-        
-            
-
-
+    cin>>n;
+    for(int i=0; i!=n; i++)
+    {
+        cout <<"Podaj a: ";
+        cin>>a;
+        iloczyn = iloczyn*a;
+    }
+    cout << iloczyn;
+    return 0;
 }
+
     
     
 
