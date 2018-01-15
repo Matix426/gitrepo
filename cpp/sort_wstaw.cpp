ec3810bd6dbdbe327dc1207cac76ccb0a7923b15
Matix426 <kinder0011@wp.pl>
2018-01-15 18:56:36 +0100

    Nieparzyste

--- cpp/sort_wstaw.cpp
+++ cpp/sort_wstaw.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void sort_wstaw(int n, int tab[])
+void sort_wstaw(int tab[], int n)
 {
     int el, k;
     for(int i=1; i<n; i++)
@@ -23,8 +23,22 @@ void sort_wstaw(int n, int tab[])
      }     
 }
 
+
 int main(int argc, char **argv)
 {
-	return 0;
-}
+    int tab[];
+    tab[0]= 4; 
+    tab[1]= 3; 
+    tab[2]= 7; 
+    tab[3]= 0; 
+    tab[4]= 2; 
+    tab[5]= 3; 
+    tab[6]= 1; 
+    tab[7]= 9; 
+    
 
+    
+    cout<<tab[i]<<;
+    
+    return 0;
+}
