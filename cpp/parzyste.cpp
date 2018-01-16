b468993276ac3a3237c7a6b33cbc2698ad1614fe
Matix426 <kinder0011@wp.pl>
2018-01-16 10:36:19 +0100

    Złożoność

---
+++ cpp/parzyste.cpp
@@ -0,0 +1,22 @@
+/*
+ * parzyste.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a;
+    cout <<"Podaj liczbę: " << endl;
+    cin >> a;
+    
+    for(i = 0; i < 100; i++)
+        if 
+    
+	
+	return 0;
+}
+
