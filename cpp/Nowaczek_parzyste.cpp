55308ac7089fb3b080dd0e358c36137b976120e1
Matix426 <kinder0011@wp.pl>
2018-01-22 12:33:10 +0100

    Zlozonosc

---
+++ cpp/Nowaczek_parzyste.cpp
@@ -0,0 +1,28 @@
+/*
+ * parzyste.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    for(int i = 0; i < 100; i += 2) 
+    {
+        int a;
+        cout << "Podaj a: ";
+        i = 2;
+    
+        if (a=i)
+            cout<<"A jest parzyste"<<endl;
+        else
+            cout<<"A jest nieparzyste"<<endl;
+    }
+            
+    
+	return 0;
+}
+
+
