8723699bca5c992d234be8f4d3e66ed2baa18103
Matix426 <kinder0011@wp.pl>
2018-01-25 08:46:01 +0100

    Dec2bin

---
+++ cpp/dec2bin.cpp
@@ -0,0 +1,21 @@
+/*
+ * dec2bin.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int liczba = 120;
+    do
+    { 
+        ; 
+        
+    }while(); // napisac zeby drukowalo
+	
+	return 0;
+}
+
