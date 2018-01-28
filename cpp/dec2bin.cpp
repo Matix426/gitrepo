0dc849cccfd14ac164220caedbe56555ceb62050
Matix426 <kinder0011@wp.pl>
2018-01-28 19:16:55 +0100

    Dec2bin

--- cpp/dec2bin.cpp
+++ cpp/dec2bin.cpp
@@ -14,7 +14,17 @@ int main(int argc, char **argv)
     { 
         ; 
         
-    }while(); // napisac zeby drukowalo
+    }while(liczba > 0);
+    for (int j = i - 1; j >= 0; j--)
+    {
+         cout << reszty [j];
+    } 
+    int j = i - 1;
+        while(j>=0)
+        {
+            cout<<reszty[j];
+            j--;
+        }
 	
 	return 0;
 }
