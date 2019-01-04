6a8669818512d02e5d6fbeeeaa8873c9f195c520
Matix426 <kinder0011@wp.pl>
2019-01-04 13:30:18 +0100

    Quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  quiz.py
-
+#  views.py
 
 from flask import Flask
 from flask import render_template
+from flask import request
+from modele import Kategoria, Pytanie, Odpowiedz
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    #return "Witaj Świecie!"
-    return render_template("index.html")
-    
-    
+    # return "Hello World!"
+    return render_template('index.html')
+
 @app.route("/lista")
 def lista():
-    return "<h1>Witaj na serwerze</h1><h2>Aplikacja Quiz</h2>"
+    pytania = Pytanie().select()
+    if not pytania.count():
+        pass
+    return render_template('lista.html', pytania=pytania)
+
+@app.route("/quiz", methods=['GET', 'POST'])
+def quiz():
     
-@app.route("/klasa")
-def klasa():
-    return "<h2>Kl3ag1</h2>"
-
-
+    print(request.form)
+    
+    if request.method == 'POST':
+        wynik = 0
+        for pid, oid in request.form.items():
+            if Odpowiedz().get(Odpowiedz.id == int(oid)).odpok:
+                wynik += 1 
+    
+    pytania = Pytanie().select().join(Odpowiedz).distinct()
+    return render_template('quiz.html', pytania=pytania)
