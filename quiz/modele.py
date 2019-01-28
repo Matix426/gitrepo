cc9687c888be399d418145005a8818070b105497
Matix426 <kinder0011@wp.pl>
2019-01-28 09:39:51 +0100

    Flask + Bootstrap

--- quiz/modele.py
+++ quiz/modele.py
@@ -29,4 +29,3 @@ class Odpowiedz(BazaModel):
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
-
--- quiz/views.py
+++ quiz/views.py
@@ -4,9 +4,10 @@
 #  views.py
 
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import render_template, request, flash, redirect, url_for
+
 from modele import Kategoria, Pytanie, Odpowiedz
+
 
 app = Flask(__name__)
 
@@ -31,7 +32,11 @@ def quiz():
         wynik = 0
         for pid, oid in request.form.items():
             if Odpowiedz().get(Odpowiedz.id == int(oid)).odpok:
-                wynik += 1 
+                wynik += 1
+                
+        flash('Poprawnych odpowiedzi: {}'.format(wynik), 'info')
+        return redirect(url_for('hello'))
     
     pytania = Pytanie().select().join(Odpowiedz).distinct()
     return render_template('quiz.html', pytania=pytania)
+
